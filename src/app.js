340560a067d47af6c9d49496a31b250c4441a0e1
methlomed <methlomed@gmail.com>
2021-09-19 14:17:35 +0700

    npm i bootstrap &co

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // main.js app.js become as entry point
-import regeneratorRuntime from "regenerator-runtime";
+import regeneratorRuntime from 'regenerator-runtime';
 import './script/component/app-bar.js';
 import main from './script/view/main.js';
 
